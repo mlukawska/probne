773f32b67dcadf4b59a27c3803740e524310e10d
Magdalena Lukawska <magdalena.lukawska96@gmail.com>
2020-09-02 19:49:50 +0200

    test 2

--- proba.py
+++ proba.py
@@ -1,1 +1,18 @@
 print ("test gita")
+#input('Press enter to continue...')
+
+
+
+IsWeekend = True
+print('Is Weekend =', IsWeekend)
+
+Temperature = 22
+print('Temperature =', Temperature)
+
+ToDoList = ''
+print('ToDoList =', ToDoList)
+
+IsHappy = IsWeekend and Temperature >= 20 and ToDoList == ''
+print('IsHappy =', IsHappy)
+
+
