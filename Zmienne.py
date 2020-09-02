6db56e85658b36e2d4d8a5c0f6ac1d668e067ea3
Magdalena Lukawska <magdalena.lukawska96@gmail.com>
2020-09-02 20:07:33 +0200

    zmienne

---
+++ Zmienne.py
@@ -0,0 +1,39 @@
+Title = 'Python'
+print('Title is ', type(Title))
+
+Version = 3
+print('Version is ', type(Version))
+
+Progress = 0.31
+print('Progress is ', type(Progress))
+
+
+
+x=1
+y=1
+z=1
+print(x,y,z)
+
+a=b=c=2
+print(a,b,c)
+
+c=3
+print(a,b,c)
+
+
+print(2+2*2)
+print(6/2*(1+2))
+print(4**3**2)  #tj. 4 do 9
+
+x=1
+x+1
+print(x)
+x=x+1
+print(x)
+x+=1
+print(x)
+x-=1
+print(x)
+
+
+
